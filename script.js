2bf960b26c4090e29c78eb8ae3d587db855d4c85
Your Name <aileen1125@gmail.com>
2023-11-27 14:55:23 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// SCROLL REVEAL (TOP TO BOTTOM)
+window.addEventListener('scroll',reveal);
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+
+      if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+
+// TESTIMONIALS SWIPER
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
