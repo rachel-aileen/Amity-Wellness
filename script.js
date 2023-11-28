52f656b77da466908331b2c29764e8381e341166
Your Name <aileen1125@gmail.com>
2023-11-27 20:23:28 -0800

    js scroll right and left

--- script.js
+++ script.js
@@ -1,72 +1,98 @@
 // SCROLL REVEAL (FADE IN)
 ;(function() {
   document.addEventListener("DOMContentLoaded", function(event) {
-    var scrollFadeInEffectEls = document.querySelectorAll(
-      ".scroll-fade-in-effect"
-    );
+  
+    function handleScroll() {
+      var scrollFadeInEffectEls = document.querySelectorAll(".scroll-fade-in-effect");
 
-    scrollFadeInInit(scrollFadeInEffectEls);
+      scrollFadeInEffectEls.forEach(element => {
+        var windowHeight = window.innerHeight;
+        var posFromTop = element.getBoundingClientRect().top;
 
-    window.addEventListener("scroll", function() {
-      scrollFadeInEffect(scrollFadeInEffectEls);
-    });
+        if (posFromTop - windowHeight <= 0) {
+          element.classList.remove("is-hidden");
+        } else {
+          element.classList.add("is-hidden");
+        }
+      });
+    }
+
+    window.addEventListener('scroll', handleScroll);
   });
-
-  function scrollFadeInInit(currentElements) {
-    var windowHeight = window.innerHeight;
-    for (var i = 0; i < currentElements.length; i++) {
-      var posFromTop = currentElements[i].getBoundingClientRect().top;
-      if (posFromTop - windowHeight > 0) {
-        currentElements[i].classList.add("is-hidden");
-      }
-    }
-  }
-
-  function scrollFadeInEffect(currentElements) {
-    var windowHeight = window.innerHeight;
-    for (var i = 0; i < currentElements.length; i++) {
-      var posFromTop = currentElements[i].getBoundingClientRect().top;
-      if (posFromTop - windowHeight <= 0) {
-        currentElements[i].classList.remove("is-hidden");
-      }
-    }
-  }
 })();
 
+// SCROLL REVEAL (RIGHT AND LEFT)
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
 // SCROLL REVEAL (TOP TO BOTTOM)
-window.addEventListener('scroll',reveal);
+window.addEventListener('scroll', reveal);
 
-  function reveal(){
-    var reveals = document.querySelectorAll('.reveal');
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
 
-    for(var i = 0; i < reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
 
-      var windowheight = window.innerHeight;
-      var revealtop = reveals[i].getBoundingClientRect().top;
-      var revealpoint = 150;
-
-      if(revealtop < windowheight - revealpoint){
-        reveals[i].classList.add('active');
-      }
-      else{
-        reveals[i].classList.remove('active');
-      }
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
     }
   }
-
-
+}
 
 // TESTIMONIALS SWIPER
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
